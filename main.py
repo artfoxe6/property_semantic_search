cddaa83a024c10e53498053938af1ce93b46b501
artfoxe6 <artfoxe6@gmail.com>
2025-07-10 09:24:14 +0000

    modify

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def prepare_data(bert: SentenceBert, num=100):
 
 
 def random_property_to_db(num=1000):
-    sdb = SqliteDB("property_dev.db")
+    sdb = SqliteDB("property.db")
     while num > 0:
         prop = Property()
         prop.generate_property()
@@ -56,6 +56,8 @@ def random_property_to_db(num=1000):
         num -= 1
 
 def sync_to_milvus():
+    vdb = VectorDB()
+    sdb = SqliteDB("property.db")
     page_size = 1000
     last_id = 0
     b = SentenceBert()
@@ -72,41 +74,48 @@ def sync_to_milvus():
             last_id = prop.id
 
 def gen_training_data(dev = False):
-    if dev:
-        sdb = SqliteDB("property_dev.db")
-    else:
-        sdb = SqliteDB()
+    sdb = SqliteDB()
     page_size = 1000
     last_id = 0
-    if dev:
-        header = ["query", "positive"]
-    else:
-        header = ["query", "positive", "negative"]
-    if dev:
-        fp = open("dev_tran_data.csv", "w", newline="", encoding="utf-8")
-    else:
-        fp = open("tran_data.csv", "w", newline="", encoding="utf-8")
+    header_dev = ["query", "positive"]
+    header = ["query", "positive", "negative"]
+
+    fp_dev = open("dev_tran_data.csv", "w", newline="", encoding="utf-8")
+    writer_dev = csv.writer(fp_dev)
+    writer_dev.writerow(header_dev)
+
+    fp = open("tran_data.csv", "w", newline="", encoding="utf-8")
     writer = csv.writer(fp)
     writer.writerow(header)
+
+    count_dev = 0
     count = 0
     while True:
         props = sdb.list(last_id, page_size)
         if not props:
             break
         for prop in props:
-            count += 1
             last_id = prop.id
             queries = prop.property_to_query_texts()
             for query in queries:
-                if dev:
-                    writer.writerow([query[1], prop.description])
-                else:
+                if count < 500000:
                     negative = prop.gen_negative_property(query[0])
                     writer.writerow([query[1], prop.description, negative])
+                    count += 1
+                else:
+                    writer.writerow([query[1], prop.description])
+                    count_dev += 1
 
         if count % 10000 == 0:
-            print(f"{count}/50000")
+            print(f"{count}/500000")
+        if count_dev % 10000 == 0:
+            print(f"dev {count}/50000")
+
+        if count >= 500000 and count_dev >= 50000:
+            break
+
     fp.close()
+    fp_dev.close()
 
 
 
@@ -115,17 +124,12 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         step = sys.argv[1]
 
-    if step == "prepare":
+    if step == "gen_property_data":
         random_property_to_db(500000)
-    if step == "prepare_dev":
-        random_property_to_db(50000)
-    elif step == "sync":
-        vdb = VectorDB()
+    elif step == "sync_to_milvus":
         sync_to_milvus()
     elif step == "gen_tran_data":
         gen_training_data()
-    elif step == "gen_dev_tran_data":
-        gen_training_data(True)
     elif step == "train":
         train_model("tran_data.csv")
     else:
