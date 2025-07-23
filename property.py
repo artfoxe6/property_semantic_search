6722f61eb40daaac93445af3a8c0257faa6e2a9c
artfoxe6 <artfoxe6@gmail.com>
2025-07-23 22:29:18 +0800

    modify

--- property.py
+++ property.py
@@ -413,7 +413,7 @@ class Property:
     # 构造负样本
     def negative_property_v2(self, mask=None):
         # 随机抛弃一个条件
-        mask = random.sample(mask, len(mask)- choice([0,1]))  # 生成新数组
+        mask = random.sample(mask, len(mask)- choice([0,len(mask)-1]))  # 生成新数组
         l = Location()
         # p = copy.deepcopy(self)
         p = Property()
